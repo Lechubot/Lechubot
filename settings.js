ce20462318c3991a25902393e95740e07a56453c
Lechubot <110015627+Lechubot@users.noreply.github.com>
2022-07-26 12:18:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,32 +54,32 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.vcardowner = ['919656509450'] //ur owner number
+global.ownername = "🇩🇬⃞͕᪳͢￫𖤐㋦͢ꪶ«᪳ⁿ͍ᵉ͍ᶻ͍ᵏ͍ᵒ͍»᪳ꫂ₇₇₇ϟ🜲➤🎈⃗͢⸙" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.socialm = "Instagram: hope_x3_nezko" //ur github or insta name
+global.location = "India, Kerala, Palakkad" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
+global.owner = ['919656509460']
+global.ownertag = '919656509460' //ur tag number
+global.botname = 'Nezko Bot MD V4' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.reactmoji = "🌚" //ur menu react emoji
+global.themeemoji = "🙈" //ur theme emoji
+global.packname = "★⃝🦋🌚എന്റെ യോക്യത എന്തെന്ന് എന്നെ സ്നേഹ᭄ച്ചവർക്കൊഴ᭄കെ വേറെയാർക്കും മനസ്സ᭄ലാക᭄ല്ല ◈᭄͡🕊️😊💜" //ur sticker watermark packname
+global.author = "🇩🇬⃞͕᪳͢￫𖤐㋦͢ꪶ«᪳ⁿ͍ᵉ͍ᶻ͍ᵏ͍ᵒ͍»᪳ꫂ₇₇₇ϟ🜲➤🎈⃗͢⸙" //ur sticker watermark author
 
 //Bot theme media
-global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
-global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
-global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
-global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.thum = fs.readFileSync("./XeonMedia/theme/20220719_213924.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./XeonMedia/theme/laurenxburch-20072022-0003.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./XeonMedia/theme/hannahxowo-20072022-0001.jpg") //ur error pic
+global.thumb = fs.readFileSync("./XeonMedia/theme/hannahxowo-20072022-0001.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['919072290207'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
